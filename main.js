ae77a87b4543a626b1456aefc1654d04fea90e96
JonLayman <133721255+JonLayman@users.noreply.github.com>
2023-05-17 05:27:13 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,45 @@
+
+var userName = 'Jon';
+console.log(userName);
+
+if (userName === 'Jon') {
+    console.log('Hello, Jon!');
+} else {
+    console.log('Hello!');
+}
+
+var userQuestion = 'Will today be a great day to learn code?';
+console.log(userQuestion);
+
+var randomNumber = Math.floor(Math.random() * 8);
+
+console.log(randomNumber);
+
+var eightBall = '';
+
+if (randomNumber === 0) {
+   console.log('It is certain'); 
+} else if 
+   (randomNumber === 1) {
+
+    console.log('It is decidedly so');
+} else if (randomNumber === 2) {
+    console.log('Reply hazy cuz im feelin lazy, try again');
+
+} else if (randomNumber === 3) {
+console.log('Cant Predict right');
+
+} else if (randomNumber === 4) {
+    console.log('Dont count on it');
+
+} else if (randomNumber === 5) {
+console.log('My sources say no but you should anyways');
+
+} else if (randomNumber === 6) {
+    console.log('Outlook not so good');
+
+} else if (randomNumber === 7) {
+  console.log('Yeah, and you should wear your sunglasses inside because your future is so bright');
+
+}
+
